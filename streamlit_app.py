aed544de91209df4412835a58b96cc27d99dc316
cs-buzz <wdd.fnl@gmail.com>
2023-01-08 10:05:29 +0800

    app4.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,9 @@ def predict(image):
   predictions = model.predict(test_image)
   scores = tf.nn.softmax(predictions[0])
   scores = scores.numpy()
-  st.write(predictions)
-  st.write(scores)
-  result = f'{class_names[np.argmax(scores)]} with a { (100 * np.max(scores)).round(2) } % confidence.' 
+  # st.write(predictions)
+  # st.write(scores)
+  result = f'Prostate Cancer: {class_names[np.argmax(scores)]} with a { (100 * np.max(scores)).round(2) } % confidence.' 
   return result
 
 
@@ -36,37 +36,3 @@ else:
     predictions = predict(image)
     st.success('Classified')
     st.write(predictions)
-
-# if file is None:
-#   st.text('Please upload an image file')
-# else:
-#   image = Image.open(file)
-#   st.image(image, use_column_width=True)
-#   predictions = import_and_predict(image, model)
-#   score = tf.nn.softmax(predictions[0])
-#   st.write(predictions)
-#   st.write(score)
-#   print(
-#   'This image most likely belongs to {} with a {:.2f} percent confidence.'
-#   .format(['Benign', 'G3', 'G4', 'G5'][np.argmax(score)], 100 * np.max(score))
-# )
-
-
-# pic = st.file_uploader('Upload an Image', type=['jpg', 'png', 'webp'])
-# if pic:
-#     st.image(pic, 'Your uploaded image')
-
-#     with st.spinner('Detecting and analyzing faces....'):
-#         pic = Image.open(pic)
-#         pic = np.array(pic)
-#         faces, age, gender = get_img_and_predict(pic)
-#         if len(faces) == 0:
-#             st.warning('No faces detected!')
-#         else:
-#             st.success('Success!! '+str(len(faces))+ ' faces detected and analyzed.')
-#             cols = st.columns(len(faces))
-#             for i in range(0, len(faces)):
-#                 with cols[i]:
-#                     st.image(faces[i])
-#                     st.write('Age: ' + str(age[i]))
-#                     st.write('Gender: ' + str(gender[i]))
